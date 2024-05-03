2e0cd88ed49928b48b69edbb19b50b35a986abb0
Danil Derkachev <derkachev.danil2002@mail.ru>
2024-05-03 16:14:16 +0300

    Initial commit

---
+++ extensions.py
@@ -0,0 +1,35 @@
+import json
+import requests
+from config import keys
+
+
+'''Моё исключение'''
+class APIException(Exception):
+    pass
+
+'''Конвертация валюты'''
+class ValuteConverter:
+    @staticmethod
+    def get_price(quote: str, base: str, amount: str):
+        if quote == base:
+            raise APIException(f'Невозможно перевести одинаковые валюты {base}')
+
+        try:
+            quote_ticker = keys[quote]
+        except KeyError:
+            raise APIException(f'Не удалось обработать валюту {quote}')
+
+        try:
+            base_ticker = keys[base]
+        except KeyError:
+            raise APIException(f'Не удалось обработать валюту {base}')
+
+        try:
+            amount = abs(float(amount))
+        except ValueError:
+            raise APIException(f'Не удалось обработать количество {amount}')
+
+        r = requests.get(f'https://min-api.cryptocompare.com/data/price?fsym={quote_ticker}&tsyms={base_ticker}')
+        total_base = json.loads(r.content)[keys[base]]
+        total_base = round(total_base, 2)  # Для удобства округляем текущий курс до двух знаков после запятой
+        return total_base * amount
---
+++ main.py
@@ -0,0 +1,43 @@
+import telebot
+from config import keys, TOKEN
+from extensions import APIException, ValuteConverter
+
+bot = telebot.TeleBot(TOKEN)  # Создание бота
+
+'''Ответ бота на команды /start и /help'''
+@bot.message_handler(commands=['start', 'help'])
+def start_help(message):
+    text = ('Чтобы начать работу с ботом введите:\n'
+            '<название валюты> <в какую валюту перевести> <количество валюты>\n'
+            'Чтобы увидеть список доступных валют введите: /values')
+    bot.reply_to(message, text)
+
+'''Ответ бота на команду /values'''
+@bot.message_handler(commands=['values'])
+def values(message: telebot.types.Message):
+    text = 'Список доступных валют: '
+    for key in keys.keys():
+        text = '\n'.join((text, key))
+    bot.reply_to(message, text)
+
+'''Ответ бота на ввод текста'''
+@bot.message_handler(content_types=['text'])
+def convert(message: telebot.types.Message):
+    try:
+        values = message.text.lower().split(' ')  # Разбиваем введённый пользователем текст по пробелам
+
+        if len(values) != 3:
+            raise APIException('Слишком много параметров')
+
+        quote, base, amount = values
+        total_base = ValuteConverter.get_price(quote, base, amount)  # Результат конвертации валют
+    except APIException as e:
+        bot.reply_to(message, f'Ошибка пользователя\n{e}')
+    except Exception as e:
+        bot.reply_to(message, f'Не удалось обработать команду\n{e}')
+    else:  # Если введённый пользователем текст не попал под вышеописанные исключения, то бот отправляет результат конвертации
+        text = f'Цена {amount} {quote} в {base}: {total_base}'
+        bot.reply_to(message, text)
+
+
+bot.polling(non_stop=True)  # Запуск бота
