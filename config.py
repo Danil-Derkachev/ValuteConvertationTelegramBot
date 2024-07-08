963b2bf3990a1274f053551b1754a51f0785ea0c
Danil Derkachev <derkachev.danil2002@mail.ru>
2024-07-08 14:39:51 +0300

    Скрыл токен

--- config.py
+++ config.py
@@ -1,4 +1,12 @@
-TOKEN = '6873825676:AAG2KtXMW3VV8NMBUl7YLksvaxVDsuSvicQ'  # Токен(пароль) моего бота
+import os
+from pathlib import Path
+from dotenv import load_dotenv
+
+
+env_path = Path('.') / '.env'
+load_dotenv(dotenv_path=env_path)
+
+TOKEN = os.getenv('TOKEN')  # Токен(пароль) моего бота
 keys = {'евро': 'EUR',          # Доступные для конвертации валюты
         'рубль': 'RUB',
         'доллар': 'USD'}
